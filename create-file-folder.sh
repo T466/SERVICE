8c31fcab539113c666e111274c51de67a8e9e884
Thiago Freitas <66418848+T466@users.noreply.github.com>
2022-03-27 14:29:42 -0300

    Update create-file-folder.sh

--- create-file-folder.sh
+++ create-file-folder.sh
@@ -2,11 +2,21 @@
 
 for (( loop=1;loop>0;loop++));do
 
+if [ ! -e "/data" ]; then
+
 hoje=$(date +%d%m%Y-%H%M%S)
 mkdir /data
 mkdir /data/folder-$hoje
 touch /data/file-hoje
 sleep 1
 
+   else
+
+hoje=$(date +%d%m%Y-%H%M%S)
+mkdir /data/folder-$hoje
+touch /data/file-hoje
+sleep 
+
+fi
 
 done 
