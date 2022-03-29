37812c875313a2d90a86ea105aa493f731c08ad0
Thiago Freitas <66418848+T466@users.noreply.github.com>
2022-03-29 07:24:46 -0300

    Update create-file-folder.sh

--- create-file-folder.sh
+++ create-file-folder.sh
@@ -7,14 +7,12 @@ if [ ! -e "/data" ]; then
 
 hoje=$(date +%d%m%Y-%H%M%S)
 mkdir /data
-mkdir /data/folder-$hoje
 ls -la /etc > /data/file-$hoje
 sleep 1
 
    else
 
 hoje=$(date +%d%m%Y-%H%M%S)
-mkdir /data/folder-$hoje
 ls -la /etc > /data/file-$hoje
 sleep 1
 
